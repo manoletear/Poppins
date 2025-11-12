8eaae0307394754fbb30cded6c267655e044f9c3
manoletear_poppins <138632822+manoletear@users.noreply.github.com>
2025-11-12 17:43:41 -0300

    fix: deploy config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Poppins/', // 👈 NECESARIO para GitHub Pages
+  base: '/Poppins/', // 👈 Necesario para GitHub Pages
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
